a5f2364cef5da6221561a641831b02c9c3ede2e7
ashwani <ashwani7kumar7@gmail.com>
2026-02-12 19:34:27 +0530

    react agent 1

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-
 from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
+from tavily import TavilyClient
 
-from schemas import AgentResponse
+tavily = TavilyClient()
 
-tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o")
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
 
 
-agent = create_agent(
-    model=llm,
-    tools=tools,
-    response_format=AgentResponse,
-)
+llm = ChatOpenAI(model="gpt-5")
+tools = [search]
+agent = create_agent(model=llm,tools=tools)
 
 
 def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
-                }
-            ]
-        }
-    )
-    # Access structured response from the agent
-    structured = result.get("structured_response", None)
-    print(structured if structured is not None else result)
-
+    print("Hello from langchain-course!")
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
+    print(result)
 
 if __name__ == "__main__":
     main()
